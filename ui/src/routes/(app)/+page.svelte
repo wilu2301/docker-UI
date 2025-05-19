14a8268b674981b2676bbca508ac7f3fab356cc1
wilu2301 <wilu2301@users.noreply.github.com>
2025-05-19 20:50:46 +0200

    Added storybook Added Navbar.stories.svelte

--- ui/src/routes/(app)/+page.svelte
+++ ui/src/routes/(app)/+page.svelte
@@ -1,18 +1,11 @@
 <script lang="ts">
-	import { ping } from '$lib/api/api';
 
-	const apiCallPromise: Promise<{ data?: any; error?: any; response: Response }> = ping();
 </script>
 
 <main>
 	<div class="dashboard">
 		<h1>Dashboard</h1>
 		<p>Welcome to the dashboard!</p>
-		{#await apiCallPromise}
-			<p>Loading</p>
-		{:then result}
-			{result.data.ping}
-		{/await}
 	</div>
 </main>
 
---
+++ ui/src/stories/Navbar.stories.svelte
@@ -0,0 +1,12 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	import Navbar from "$root/components/Navbar.svelte"
+
+	const { Story } = defineMeta({
+		title: 'Navigation',
+		component: Navbar,
+	});
+</script>
+
+<Story name="Navigation" />
