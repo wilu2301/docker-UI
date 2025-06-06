b39e81c391f65d340ba816ea5bef4b5825c3eb2d
wilu2301 <wilu2301@users.noreply.github.com>
2025-06-06 22:14:49 +0200

    Added Ui layout for CodeField.svelte

--- ui/src/routes/(app)/compose/detail/[app]/CodeField.svelte
+++ ui/src/routes/(app)/compose/detail/[app]/CodeField.svelte
@@ -1,17 +1,119 @@
 <script lang="ts">
 	import Monaco from 'svelte-monaco';
+	import { File, GitCommitHorizontal } from '@lucide/svelte';
+
+	let git: boolean = $state(false);
 </script>
 
-<div class="editor-container">
-	<Monaco theme="cobalt" value="" options={{ language: 'yaml', automaticLayout: true }} />
+<div class="collum">
+	<div class="top">
+		<div class="item">
+			<File /><span>test_app.yaml</span>
+		</div>
+		{#if git}
+			<div class="item">
+				<GitCommitHorizontal /> <span>Example commit</span>
+			</div>
+		{/if}
+	</div>
+
+	<div class="editor-container">
+		<Monaco theme="cobalt" value="" options={{ language: 'yaml', automaticLayout: true }} />
+	</div>
+	<div class="bottom">
+		{#if git}
+			<div class="git">
+				<input type="text" placeholder="Commit message" />
+				<button>Commit</button>
+			</div>
+		{:else}
+			<button>Save</button>
+		{/if}
+	</div>
 </div>
 
-<style>
-	.editor-container {
-		height: 300px;
+<style lang="scss">
+	@use '$root/style/pallet';
+	.collum {
 		width: 100%;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
 
-		border-radius: 2px;
-		border: 2px solid rgba(255, 255, 255, 0.1);
+		.top {
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+			justify-content: space-between;
+
+			.item {
+				display: flex;
+				flex-direction: row;
+				align-items: center;
+			}
+		}
+
+		.bottom {
+			width: 100%;
+
+			display: flex;
+			flex-direction: row;
+			justify-content: right;
+
+			.git {
+				width: 100%;
+				display: flex;
+				flex-direction: row;
+
+				border: solid 1px pallet.$secondary;
+				border-radius: 16px;
+
+				input {
+					width: 100%;
+
+					background: none;
+					color: pallet.$white;
+
+					text-align: center;
+
+					border: none;
+					outline: none;
+				}
+
+				button {
+					padding: 1rem;
+					background: pallet.$secondary;
+					border: none;
+					border-radius: 0 16px 16px 0;
+
+					transition: background 0.5s;
+
+					&:hover {
+						background: pallet.$white;
+					}
+				}
+			}
+
+			button {
+				padding: 1rem;
+				background: pallet.$secondary;
+				border: none;
+				border-radius: 16px;
+
+				transition: background 0.5s;
+
+				&:hover {
+					background: pallet.$white;
+				}
+			}
+		}
+
+		.editor-container {
+			height: 300px;
+			width: 100%;
+
+			border-radius: 2px;
+			border: 2px solid rgba(255, 255, 255, 0.1);
+		}
 	}
 </style>
